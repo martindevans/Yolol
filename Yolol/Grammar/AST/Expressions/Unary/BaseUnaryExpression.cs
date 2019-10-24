2e79421983674c49b04aed91a91dfe3b61dea5b3
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-10-24 20:48:39 +0200

    Add BaseUnaryExpression
    Create the class BaseUnaryExpression to standardize all of the AST's unary expressions, thus making our code more expandable and allowing us to adhere to the DRY (Don't Repeat Yourself) philosophy.

---
+++ Yolol/Grammar/AST/Expressions/Unary/BaseUnaryExpression.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Yolol.Execution;
+using Type = Yolol.Execution.Type;
+
+namespace Yolol.Grammar.AST.Expressions.Unary
+{
+    public abstract class BaseUnaryExpression: BaseExpression
+    {
+        [NotNull] public BaseExpression Parameter { get; }
+
+        public override bool IsConstant => Parameter.IsConstant;
+
+        public override bool IsBoolean => false;
+
+        protected BaseUnaryExpression([NotNull] BaseExpression parameter)
+        {
+            Parameter = parameter;
+        }
+
+        protected abstract Value Evaluate([NotNull] string parameterValue);
+
+        protected abstract Value Evaluate(Number parameterValue);
+
+        public override Value Evaluate([NotNull] MachineState state)
+        {
+            var parameterValue = Parameter.Evaluate(state);
+
+            switch (parameterValue.Type)
+            {
+                case Type.String:
+                    return Evaluate(parameterValue.String);
+                default:
+                    return Evaluate(parameterValue.Number);
+            }
+        }
+    }
+}
