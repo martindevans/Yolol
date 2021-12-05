9760aa81114ed7faf6dd378401e280da05a5ca4d
Martin Evans <martindevans@gmail.com>
2021-12-05 16:50:16 +0000

    Forgot a file, oops

---
+++ Yolol/Execution/SaturatingCounters.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Yolol.Execution
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct SaturatingCounters
+    {
+        [FieldOffset(0)] public SaturatingByte ZeroCount;
+        [FieldOffset(1)] public SaturatingByte OnesCount;
+
+        public SaturatingCounters(SaturatingByte z, SaturatingByte o)
+        {
+            ZeroCount = z;
+            OnesCount = o;
+        }
+
+        public static SaturatingCounters operator +(SaturatingCounters a, SaturatingCounters b)
+        {
+            return new SaturatingCounters(
+                a.ZeroCount + b.ZeroCount,
+                a.OnesCount + b.OnesCount
+            );
+        }
+
+        public static SaturatingCounters operator -(SaturatingCounters a, SaturatingCounters b)
+        {
+            return new SaturatingCounters(
+                a.ZeroCount - b.ZeroCount,
+                a.OnesCount - b.OnesCount
+            );
+        }
+    }
+}
